e9a66578bd9d157acfd3d93a7c9095992532c082
Dave Bourke <squire40@hotmail.com>
2019-01-31 18:44:49 -0500

    Validator class

---
+++ MergeConflicts/Validator.cs
@@ -0,0 +1,20 @@
+﻿namespace MergeConflicts
+{
+    public class Validator : IValidator
+    {
+        public bool IsValidDate(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsValidInt(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsValidString(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
